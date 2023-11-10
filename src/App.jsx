faa1ff0b93c01514e7887cfb67fd7dda609f2752
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2023-11-10 11:37:52 -0300

    feat(todo): add project ToDo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import TitleApp from './components/Title_App';
-import PropTypes from 'prop-types'
 
 
+/*
 function App() {
   const [newItem, setNewItem] = useState(""); // Use "newItem" em vez de "todo"
   const [items, setItems] = useState([]);
@@ -25,35 +25,20 @@ function App() {
     const newArray = items.filter(item => item.id !== id);
     setItems(newArray)
   }
+  */
 
-  return (
-    <div className='app'>
-      <TitleApp />
 
-      <input
-      className='InputTag'
-        type="text"
-        placeholder='Adicione um item aqui...'
-        value={newItem} // Use "newItem" em vez de "todo"
-        onChange={change => setNewItem(change.target.value)} // Corrija a função de callback
-      />
-      <button onClick={addItem}>Add</button> {/* Adicione um botão para adicionar */}
-      <ul>
-        {items.map(item=>{
-          return(
-            <li key={item.id}>{item.value} <button onClick={()=>deleteItem(item.id)}></button></li>
-
-          )
-        })}
-      </ul>  
-    </div>
-
-    
-  );
-}
+  const App = () => {
+    return (
+      <div>
+        <TitleApp />
+      </div>
+    );
+  };
+  
+  export default App;
 
 
 
 
-export default App;
 
